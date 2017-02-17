12dec05d96a033d3e2c4ed6d0495a2cb72285105
Tianyi Ma <tn.ma@outlook.com>
2017-02-17 00:44:02 -0600

    add remote cursors

--- static/app.js
+++ static/app.js
@@ -16,7 +16,7 @@ editor.on('change', function() {
   $('#preview').html(marked(editor.getValue()));
 });
 editor.selection.on('changeCursor', function() {
-  socket.emit('changeCursor', { socketId: socket.id, value: editor.getCursorPosition() })
+  socket.emit('changeCursor', { socketId: socket.id, value: editor.getCursorPosition() });
 });
 editor.commands.addCommand({
   name: 'saveFile',
@@ -39,25 +39,95 @@ socket.on('change', function(data) {
   }
 });
 socket.on('changeCursor', function(data) {
-  editor.moveCursorToPosition(data.value);
+  if (data.socketId == socket.id) {
+    editor.moveCursorToPosition(data.value);
+  }
+});
+socket.on('changeRemoteCursor', function(data) {
+  if (data.socketId != socket.id) {
+    updateRemoteCursor(data.socketId, data.value);
+  }
 });
 socket.on('names', function(names) {
   console.log(names);
   $('#connected').html('');
   for (var nameIdx in names) {
     var name = names[nameIdx];
-    $('#connected').append('<span class="active ' + colors[name.color] + ' item">' + name.name + '</span>');
+    $('#connected').append('<span class="active ' + colors[name.colorId] + ' item">' + name.name + '</span>');
+    if (name.socketId.split('#')[1] != socket.id) {
+      addRemoteCursor(name.socketId.split('#')[1], name.colorId, name.name, name.cursorPosition);
+    }
   }
+});
+socket.on('bye', function(socketId) {
+  removeRemoteCursor(socketId);
 });
 
 $('#share').click(function() {
   $('.ui.modal').modal('show');
 });
 $('#copy-button').click(function() {
-  var textArea = document.createElement("textarea");
+  var textArea = document.createElement('textarea');
   textArea.value = $('#copy-url').html();
   document.body.appendChild(textArea);
   textArea.select();
   document.execCommand('copy');
   textArea.remove();
 });
+
+var marker = {};
+marker.cursors = {};
+marker.update = function(html, markerLayer, session, config) {
+  var start = config.firstRow;
+  var end = config.lastRow;
+  var cursors = this.cursors;
+  for (var i in cursors) {
+    console.log(this.cursors[i]);
+    var pos = this.cursors[i].cursorPosition;
+    var color = colors[this.cursors[i].colorId];
+    if (pos.row < start) {
+      continue;
+    }
+    else if (pos.row > end) {
+      break;
+    }
+    else {
+      var screenPos = session.documentToScreenPosition(pos);
+      var height = config.lineHeight;
+      var width = config.characterWidth;
+      var top = markerLayer.$getTop(screenPos.row, config);
+      var left = markerLayer.$padding + screenPos.column * width;
+      html.push('<div class="' + color + '-cursor remote-cursor" style="',
+        'height:', height, 'px;', 'top:', top, 'px;',
+        'left:', left, 'px; width:', width, 'px"></div>');
+    }
+  }
+}
+marker.redraw = function() {
+  this.session._signal('changeFrontMarker');
+}
+marker.addCursor = function() {
+  marker.redraw();
+}
+marker.session = editor.session;
+marker.session.addDynamicMarker(marker, true);
+
+function addRemoteCursor(socketId, colorId, name, cursorPosition) {
+  var cursorObj = {};
+  cursorObj.colorId = colorId;
+  cursorObj.name = name;
+  cursorObj.cursorPosition = cursorPosition;
+  marker.cursors[socketId] = cursorObj;
+  marker.redraw();
+}
+
+function updateRemoteCursor(socketId, cursorPosition) {
+  var cursor = marker.cursors[socketId];
+  cursor.cursorPosition = cursorPosition;
+  marker.redraw();
+}
+
+function removeRemoteCursor(socketId) {
+  marker.cursors[socketId] = null;
+  delete marker.cursors[socketId];
+}
