bddf9a6b1cf184d2db43d480e9515cb2f49c0caa
Tianyi Ma <tn.ma@outlook.com>
2017-02-20 19:50:29 -0600

    slide app

--- static/slide.js
+++ static/slide.js
@@ -1,1 +1,23 @@
 prodName = 'Flashslide';
+
+Reveal.initialize({
+  help: false,
+  dependencies: [
+    { src: '/reveal.js/plugin/markdown/marked.js' },
+    { src: '/reveal.js/plugin/markdown/markdown.js' },
+    { src: '/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+  ]
+});
+
+editor.on('change', function() {
+  $('.slides').html('');
+  var slides = editor.getValue().split('#HSLIDE');
+  for (var slideIdx in slides) {
+    var slide = slides[slideIdx];
+    $('.slides').append('<section data-markdown><script type="text/template">' + slide + '</script></section>');
+  }
+  RevealMarkdown.convertSlides();
+  var currentIdx = Reveal.getIndices();
+  Reveal.sync();
+  Reveal.slide(currentIdx.h, currentIdx.v);
+});
